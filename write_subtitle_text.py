82f6e8ee18a18c5e4a4b4f08eb031794bce9345d
yasai-nodame <135739623+yasai-nodame@users.noreply.github.com>
2024-05-12 21:45:35 +0900

    ファイル破損の原因を改善した

--- write_subtitle_text.py
+++ write_subtitle_text.py
@@ -2,6 +2,7 @@ import speech_recognition as sr
 import os
 import asyncio
 import ffmpeg
+import aiofiles
 
 import video_duration
 import video_path
@@ -11,52 +12,45 @@ import deepl_translation
 # 音声からsrtファイルを作成する
 ###############################
 async def extracted_from_audio(audio_file, video_file, count):
-    def text_extraction_srt():
+    subtitle_file = os.path.join(video_path.os.environ.get('SRT_DIRECTORY'), f'subtitle{count}.srt')
+    
+    try:
+        r = sr.Recognizer()
         
-        global first_frequency
-        
-        subtitle_file = os.path.join(video_path.os.environ.get('SRT_DIRECTORY'), f'subtitle{count}.srt')
-        
-        try:
-            r = sr.Recognizer()
+        with sr.AudioFile(audio_file) as source:
+            audio_data = r.record(source)
+
+        text = r.recognize_google(audio_data, language='en_US')
+        translation_text = await deepl_translation.translation(text)
+
+        # 字幕をテキストファイルに随時追加していく
+        with open(subtitle_file, 'w', encoding='utf-8') as f:
+            f.write(f'{count + 1}\n')
+            f.write(f'{await video_duration.first_format(video_duration.get_video_duration(video_file))}\n')
+            f.write(translation_text + '\n')
             
-            with sr.AudioFile(audio_file) as source:
-                audio_data = r.record(source)
+    except Exception as e:
+        print(f'Error: {e}')
+        pass
 
-            text = r.recognize_google(audio_data, language='en_US')
-            translation_text = asyncio.run(deepl_translation.translation(text))
-
-            # 字幕をテキストファイルに随時追加していく
-            with open(subtitle_file, 'w', encoding='utf-8') as f:
-                f.write(f'{count + 1}\n')
-                f.write(f'{asyncio.run(video_duration.first_format(video_duration.get_video_duration(video_file)))}\n')
-                f.write(translation_text + '\n')
-        except Exception as e:
-            print(f'Error: {e}')
-            pass
-    await asyncio.to_thread(text_extraction_srt)
-
-first_frequency = True
 
 #################################################################################################################
 # 動画、音声、字幕等の複数のメディアストリームを格納できるmkv拡張子に変換する こうすることでライブ配信に字幕がつけれる
 #################################################################################################################
 async def transform_mkv(video, audio, subtitle, count):
-    def ts_to_mkv():
-        input_video = ffmpeg.input(video)
-        input_audio = ffmpeg.input(audio)
-        
-        if not os.path.exists(subtitle):
-            output = (
-            ffmpeg.output(input_video['v'], input_audio['a'], os.path.join(video_path.os.environ.get('MKV_DIRECTORY'), f'output{count}.mkv'), c='copy')
+    input_video = ffmpeg.input(video)
+    input_audio = ffmpeg.input(audio)
+    
+    if not os.path.exists(subtitle):
+        output = (
+        ffmpeg.output(input_video['v'], input_audio['a'], os.path.join(video_path.os.environ.get('MKV_DIRECTORY'), f'output{count}.mkv'), c='copy')
+    )
+    else:
+        input_subtitle = ffmpeg.input(subtitle)
+        output = (
+            ffmpeg.output(input_video['v'], input_audio['a'], input_subtitle['s'], os.path.join(video_path.os.environ.get('MKV_DIRECTORY'), f'output{count}.mkv'), c='copy')
         )
-        else:
-            input_subtitle = ffmpeg.input(subtitle)
-            output = (
-                ffmpeg.output(input_video['v'], input_audio['a'], input_subtitle['s'], os.path.join(video_path.os.environ.get('MKV_DIRECTORY'), f'output{count}.mkv'), c='copy')
-            )
-        
-        ffmpeg.run(output)
-    await asyncio.to_thread(ts_to_mkv)
+    
+    ffmpeg.run(output)
 
 
