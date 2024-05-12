0b08fe734ca207809e79f1dd3bb2cf10b7e5f9c7
yasai-nodame <135739623+yasai-nodame@users.noreply.github.com>
2024-05-12 21:44:15 +0900

    ファイル破損だった原因は改善した

--- m3u8_download.py
+++ m3u8_download.py
@@ -4,10 +4,10 @@ import m3u8
 import os
 import aiofiles
 from moviepy.editor import VideoFileClip
+import time
 
 import write_subtitle_text
 import video_path
-import file_remove
 
 
 #############################################################
@@ -23,42 +23,42 @@ async def response_valid(url):
 # ライブ配信のm3u8ファイルを取得し、tsファイルを取得していく
 ##########################################################  # これが非同期で常に実行してるので、ファイル削除をするときにプロセス使用中とでる。　同期処理にしたらどうなるか。
 async def download_ts_segments(playlist_url, output_dir, count, m3u8_files, i):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(playlist_url) as response:
-            playlist = m3u8.loads(await response.text())
-            for segment in playlist.segments:
-                ts_url = segment.uri
-                if not ts_url in m3u8_files:
-                    ts_filename = f'file{count}.ts'
-                    output_path = os.path.join(output_dir, ts_filename)
-                    async with session.get(ts_url) as ts_response:
-                        async with aiofiles.open(output_path, mode='wb') as f:
-                            await f.write(await ts_response.read())
-                            print(f'Downloaded: {ts_url}')
-                        count += 1
-                        m3u8_files.append(ts_url)
-                        
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(playlist_url) as response:
+                playlist = m3u8.loads(await response.text())
+                for segment in playlist.segments:
+                    ts_url = segment.uri
+                    if not ts_url in m3u8_files:
+                        ts_filename = f'file{count}.ts'
+                        output_path = os.path.join(output_dir, ts_filename)
+                        async with session.get(ts_url) as ts_response:
+                            async with aiofiles.open(output_path, mode='wb') as f:
+                                await f.write(await ts_response.read())
+                                print(f'Downloaded: {ts_url}')
+                            count += 1
+                            m3u8_files.append(ts_url)
+                            
             return count
+    except Exception as e:
+        print('エラーが発生しました。', e)
 
 
 ###################################################
 # 取得したtsファイルから音声だけを切り出して取得する
 ###################################################
 async def extract_audio_async(video_file, audio_file):
-    def extract_audio():
-        # mp4ファイルから音声を抽出して保存する
-        clip = VideoFileClip(video_file)
-        audio_clip = clip.audio
-        audio_clip.write_audiofile(audio_file)
-        
-    await asyncio.to_thread(extract_audio)
+    # tsファイルから音声を抽出して保存する
+    clip = VideoFileClip(video_file)
+    audio_clip = clip.audio
+    audio_clip.write_audiofile(audio_file)
 
 
 async def main():
     url = video_path.os.environ.get('TWICH_URL')
     count = 0 
     i = 0
-    
+    global processing_time
     subtitle_srt = os.path.join(video_path.os.environ.get('SRT_DIRECTORY'), f'subtitle{i}.srt')
     
     output_dir = video_path.os.environ.get('VIDEO_FILE_PATH')
@@ -74,7 +74,7 @@ async def main():
             continue
         else:
             # ビデオと音声を切り離す
-            await extract_audio_async(video_file, audio_file)
+            processing_time = await extract_audio_async(video_file, audio_file)
             
             # 音声ファイルからテキスト抽出、ビデオファイルから時間抽出、srtファイル生成
             await write_subtitle_text.extracted_from_audio(audio_file, video_file, i)
@@ -90,6 +90,7 @@ async def main():
         # ライブ配信が終わったらbreakする
         if status_valid != 200:
             print('ライブ配信が終わったので、終了します。')
+            time.sleep(5)
             break 
         
         i += 1
